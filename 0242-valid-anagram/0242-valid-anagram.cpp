71c2faf7dd6a287dc315abc5f48f7ce73d062c53
parthaksingh <152584992+parthaksingh@users.noreply.github.com>
2026-08-19 23:31:37 +0530

    Time: 4 ms (33.98%), Space: 9.9 MB (11.54%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+        unordered_map<char, int> mp;
+        for(int i = 0; i<s.length(); i++){
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+
+        for(auto x : mp){
+            if(x.second != 0){
+                return false;
+            }
+        }
+        return true; 
+    }
+};
