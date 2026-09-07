5ed9c997f7f4ea86341256996f0446931182964b
Parthak Singh <152584992+parthaksingh@users.noreply.github.com>
2026-09-07 18:38:35 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.27%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+       int l = 0;
+       int r = x;
+       int ans = 0;
+
+       while(l <= r){
+        long long mid = l + (r - l) / 2;
+        if(mid * mid <= x){
+            ans = mid;
+            l = mid + 1;
+        }else{
+            r = mid - 1;
+        }
+       }
+       return ans;
+    }
+};
