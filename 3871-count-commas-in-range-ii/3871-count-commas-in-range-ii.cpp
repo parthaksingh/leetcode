e16db76c654e228f0cf9e7f5f0e52eb2fa5edba5
Parthak Singh <152584992+parthaksingh@users.noreply.github.com>
2026-09-09 19:53:21 +0530

    Time: 0 ms (100%), Space: 9.1 MB (19.2%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -5,20 +5,19 @@ public:
         ll result = 0;
         ll start = 1000;
         ll commas = 1;
-
         while(start <= n){
-           ll lastno = start*1000 - 1;
-           if(lastno > n)
-            lastno = n;
-
-            ll countofno = lastno - start + 1;
+            // result+=(n - start) + 1;
+            ll end = start*1000-1;
+            if(end > n){
+                end = n;
+            }
+            ll countofno = end - start + 1;
             result += countofno*commas;
 
-            start *= 1000;
-            commas++;
 
+            start*= 1000;
+            commas++;
         }
         return result;
-
     }
 };
