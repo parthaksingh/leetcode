8497986287e34110a1818a965f63e4e7420961ac
Parthak Singh <152584992+parthaksingh@users.noreply.github.com>
2026-09-11 14:22:10 +0530

    Time: 45 ms (69.8%), Space: 29 MB (73%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
         vector<vector<int>> ans;
+
         sort(nums.begin(), nums.end());
-        for(int i = 0; i<nums.size()-2;i++){
-            if(i > 0 && nums[i] == nums[i-1]){
+
+        for(int i = 0; i < n - 2; i++){
+
+            if(i > 0 && nums[i] == nums[i-1])
                 continue;
-            }
+            int j = i + 1, k = n - 1;
 
-                int left = i+1;
-                int right = nums.size() - 1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
 
-                while(left < right){
-                    long long sum = (long long)nums[i] + nums[left] + nums[right];
-                    if(sum == 0){
-                        ans.push_back({nums[i],nums[left], nums[right]});
-
-                        while(left<right && nums[left]==nums[left +1]){
-                            left++;
-                        }
-                        while(left<right && nums[right]==nums[right - 1]){
-                            right--;
-                        }
-                        left++;
-                        right--;
-                    }else if(sum<0){
-                        left++;
-                    }else{
-                        right--;
+                if(sum > 0){
+                    k--;
+                }else if(sum < 0){
+                    j++;
+                }else{
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    
+                    while(j < k && nums[j] == nums[j-1]){
+                        j++;
                     }
-
                 }
-            
+            }
         }
         return ans;
     }
