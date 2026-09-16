a1ec0fd4ae7af8f72b88464c6b435fc3a97c96d1
Parthak Singh <152584992+parthaksingh@users.noreply.github.com>
2026-09-16 18:10:19 +0530

    Time: 3 ms (62.89%), Space: 13.6 MB (35.04%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        if(k > 1){
+            sort(s.begin(), s.end());
+            return s;
+        }
+
+        string result = s;
+        int n = s.size();
+        for(int i = 1;i < n;i++){
+
+            string temp = s.substr(i) + s.substr(0, i);
+            result = min(temp, result);
+        }
+        return result;
+    }
+};
